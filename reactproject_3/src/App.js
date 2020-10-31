18ddaf67aa71503de30c042d9262c42c70b8af5e
your_github_ <your_email@example.com>
2020-11-01 01:09:59 +0200

    Project_3

--- reactproject_3/src/App.js
+++ reactproject_3/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Link}from "react-router-dom";
 import axios from 'axios';
 import Home from './Component/Home';
-import Abouts from './Component/Abouts';
 import Users from './Component/Users';
 import Data from './Component/DataBase';
 import './App.css';
@@ -12,66 +11,15 @@ export default class App extends Component {
    super(props);
    this.state={
      tasks:[],
-     data:[],
+     data:"",
      isLoggedIn: false
    }
  }
- getAllUsers = () => {
-  axios
-    .get('http://localhost:5000/Users/Get')
-    .then((response) => {
-      console.log('DATA: ', response.data);
-      this.setState({tasks: response.data}) 
-    })
-    .catch((err) => {
-      console.log('RESULT: ', err);
-    });
-};
-getAllDelete = () => {
-  axios
-    .delete('http://localhost:5000/Users/Delete')
-    .then((response) => {
-      console.log('DATA: ', response.data);
-      const newArray=[...this.state.tasks];
-      newArray.shift(response.data);
-      this.setState({tasks:newArray});
 
-    })
-    .catch((err) => {
-      console.log('RESULT: ', err);
-    });
-};
-getAllUpdate = () => {
-  axios
-    .put('http://localhost:5000/Users/Update')
-    .then((response) => {
-      console.log('DATA: ', response.data);
-      const newArray=[...this.state.tasks];
-      this.setState({tasks:newArray});
-
-    })
-    .catch((err) => {
-      console.log('RESULT: ', err);
-    });
-};
-getAllCreate = () => {
-  axios
-    .post('http://localhost:5000/Users/Create')
-    .then((response) => {
-      console.log('DATA: ', response.data);
-      const newArray=[...this.state.tasks];
-       newArray.push(response.data)
-      this.setState({tasks:newArray});
-
-    })
-    .catch((err) => {
-      console.log('RESULT: ', err);
-    });
-};
 
 
   render() {
-    
+   
     return (
       
       <Router>
@@ -80,15 +28,13 @@ getAllCreate = () => {
         <div className='main'>
           <nav>
               <Link to="/home">Home</Link>
-              <Link to="/about">About</Link>
               <Link to="/users">Users</Link>
               <Link to="/data">Data bace</Link>
           </nav>
           </div>
           <Route exact path='/home' component={Home}></Route>
-        <Route exact path='/about' component={Abouts}></Route>
         <Route  path='/users' component={Users}>
-        <Users get={this.getAllUsers} up={this.getAllUpdate} Create={this.getAllCreate} Delete={this.getAllDelete} /></Route> 
+        </Route> 
         <Route exact path='/data' component={Data}></Route> 
 </div>  
        </Router>
--- reactproject_3/src/Component/Users.js
+++ reactproject_3/src/Component/Users.js
@@ -1,47 +1,101 @@
 import React, { Component } from 'react'
-
+import Date from './DataBase'
+import axios from 'axios';
 export default class Users extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          Databace:[],
+          textInput: '',
+        };
+      }
+      
+      getAllUsers = () => {
+        axios
+          .get('http://localhost:5000/Users/Get')
+          .then((response) => {
+            console.log('DATA: ', response.data);
+            this.setState({Databace: response.data}) 
+          })
+          .catch((err) => {
+            console.log('RESULT: ', err);
+          });
+      };
+      getAllDelete = () => {
+        axios
+          .delete('http://localhost:5000/Users/Delete')
+          .then((response) => {
+            console.log('DATA: ', response.data);
+            const newArray=[...this.state.Databace];
+            newArray.shift(response.data);
+            this.setState({Databace:newArray});
+      
+          })
+          .catch((err) => {
+            console.log('RESULT: ', err);
+          });
+      };
+      getAllCreate = (newusers) => {
+        const newus={email:newusers,name:newusers,password:newusers,phoneNumber:newusers}
+        axios
+        
+          .post('http://localhost:5000/Users/Create',newus)
+          .then((response) => {
+            console.log('DATA: ', response.data);
+            const newArray=[...this.state.Databace];
+             newArray.push(response.data)
+            this.setState({Databace:newArray});
+      
+          })
+          .catch((err) => {
+            console.log('RESULT: ', err);
+          });
+      };
+      getAllUpdate = () => {
+        axios
+          .put('http://localhost:5000/Users/Update')
+          .then((response) => {
+            console.log('DATA: ', response.data);
+            const newArray=[...this.state.Databace];
+            this.setState({Databace:newArray});
+      
+          })
+          .catch((err) => {
+            console.log('RESULT: ', err);
+          });
+      };
   
-    TextChange=(e)=>{
-        this.setState({date:e.target.value})
-     }
-     getButtton=()=>{
-        this.props.get(this.state.tas)
-     }
-     UpdateButtton=()=>{
-        this.props.up(this.state.date)
-     }
-     CreateButtton=()=>{
-        this.props.Create(this.state.date)
-     }
-     DeleteButtton=()=>{
-        this.props.Delete(this.state.date)
-     }
+      change=(e)=>{
+       this.setState({Databace:e.target.value});
+   }
     render() {
+        
+         
         return (
             
             <div className="Users">
                 <h1>Users Information</h1>
                 <table>  
                     <tr>
-                <td><input onChange={this.TextChange} type='Email' placeholder="Email"></input></td>
+                <td><input onChange={this.change} type='Email' placeholder="Email"></input></td>
                 </tr>
                     <tr>
-                <td><input onChange={this.TextChange} type='text' placeholder="Name"></input></td>
+                <td><input onChange={this.change} type='text' placeholder="Name"></input></td>
                 </tr>
                     <tr>
-                <td><input onChange={this.TextChange} type='Password' placeholder="Password"></input></td>
+                <td><input onChange={this.change} type='Password' placeholder="Password"></input></td>
                 </tr>
                     <tr>
-                <td><input onChange={this.TextChange} type='Number' placeholder="Number Phone"></input></td>
+                <td><input onChange={this.change} type='Number' placeholder="Number Phone"></input></td>
                 </tr>
                </table>
-               <button onClick={this.getButtton}>Get</button>
-               <button onClick={this.UpdateButtton}>Update</button>
-               <button onClick={this.CreateButtton}>Create</button>
-               <button onClick={this.DeleteButtton}>Delete</button>
+               <button onClick={this.getAllUsers}>Get</button>
+               <button onClick={this.getAllUpdate}>Update</button>
+               <button onClick={this.getAllCreate}>Create</button>
+               <button onClick={this.getAllDelete}>Delete</button>
                <button onClick={()=>{this.props.history.push('/home')}}>Go To Home</button>
-               <button onClick={()=>{this.props.history.push('/about')}}>Go To About</button>
+               
+              
             </div>
             
         )
